142e06a7d7b76f3e1551621d5577672ff611288c
Bernard Roche <bjroche@protonmail.com>
2017-03-02 17:26:46 +0000

    Interim Update via script.

--- scarpa_const.js
+++ scarpa_const.js
@@ -52,7 +52,30 @@ switch(wlang) {
 
 
 exports.gethomePara2 = function() {
+switch(wlang) {
+    case 'en':
   return 'Over four generations we have acquired an enviable reputation for the most excellent made to order service, enabling our clients to compliment their wardrobe with the vast range of styles we offer for every occasion.';
+        break;
+    case 'fr':
+        return 'French: Welcome to La Bella Scarpa';
+        break;
+    case 'de':
+        return 'German: Welcome to La Bella Scarpa';
+        break;
+    case 'it':
+        return 'Da oltre 120 anni le calzature de La Bella Scarpa sono sinonimo di qualità. La nostra è una attività a conduzione familiare con sede in Provenza nelle vicinanze di Romarins. ';
+        break;
+    case 'es':
+        return 'Spanish: Welcome to La Bella Scarpa';
+        break;
+
+    default:
+  return 'Over four generations we have acquired an enviable reputation for the most excellent made to order service, enabling our clients to compliment their wardrobe with the vast range of styles we offer for every occasion.';
+}
+
+
+
+
 };
 
 exports.gethomePara3 = function() {
