aa3106d86b259becf1470714d16a4b5022141676
Bernard Roche <bjroche@protonmail.com>
2017-03-02 11:16:32 +0000

    Interim Update via script.

---
+++ newrelic.js
@@ -0,0 +1,30 @@
+'use strict'
+
+/**
+ * New Relic agent configuration.
+ *
+ * See lib/config.defaults.js in the agent distribution for a more complete
+ * description of configuration variables and their potential values.
+ */
+exports.config = {
+  /**
+   * Array of application names.
+   */
+  app_name: ['My Application'],
+  /**
+   * Your New Relic license key.
+   */
+  license_key: 'license key here',
+  /**
+   * Your New Relic Agent Beta token.
+   */
+  beta_token: 'b4c9dfcbd1dd2b746a6a2c97b0b12f722011575d',
+  logging: {
+    /**
+     * Level at which to log. 'trace' is most useful to New Relic when diagnosing
+     * issues with the agent, 'info' and higher will impose the least overhead on
+     * production applications.
+     */
+    level: 'info'
+  }
+}
